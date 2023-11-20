0ee7128b9b210b0c58741a785b9d559c306f8cda
Fulcrum04 <148826384+Fulcrum04@users.noreply.github.com>
2023-11-20 02:42:40 -0300

    Update routes.py

--- comunidade/routes.py
+++ comunidade/routes.py
@@ -14,22 +14,22 @@ def home():
         #filtrar posts de acordo com o IMC do usuário
         if current_user.IMC < 18.6:
             tipo = 1
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         elif current_user.IMC < 25:
             tipo = 2
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         elif current_user.IMC < 30:
             tipo = 3
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         elif current_user.IMC < 35:
             tipo = 4
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         elif current_user.IMC < 40:
             tipo = 5
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         else:
             tipo = 6
-            posts = Links.query.filter_by("tipo" == tipo)
+            posts = Links.query.filter_by(tipo=tipo)
         return render_template('home_log.html', posts=posts, tipo=tipo)
     else:
         return render_template("home.html")
